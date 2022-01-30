9ca750c33184b55d3bbb07960a49d6f67207b37c
bagus354 <bagus.addin354@gmail.com>
2022-01-30 21:48:03 +0700

    test deploy di heroku

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
           <br />
-          ok sudah ditambah
+          ok sudah ditambah sudah di deploy
         </p>
         <a
           className="App-link"
