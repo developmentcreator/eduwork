2900f9cdf0020c8871c1935d2dc7a1d23f48ded7
bagus354 <bagus.addin354@gmail.com>
2022-05-17 14:01:15 +0700

    update E-commerce

--- ComputerShop-Ecommerce/frontend/src/components/TopBar/index.jsx
+++ ComputerShop-Ecommerce/frontend/src/components/TopBar/index.jsx
@@ -1,47 +1,71 @@
 import React from "react";
 import { Navbar, NavbarBrand, Nav, NavDropdown } from "react-bootstrap";
 import { Container, Form, FormControl, Button } from "react-bootstrap";
-import Login from "../Profile";
-import Cart from "../Order";
+import { useState, useEffect } from "react";
+import { IndexLinkContainer, LinkContainer } from 'react-router-bootstrap'
+import { getCategories } from '../../app/api/product'
+import { useDispatch, useSelector } from 'react-redux'
+import { setCategory, setKeyword } from '../../app/features/Product/actions'
+import { totalItemCart } from '../../utils'
 
+export default function TopBar() {
+  const [categories, setCategories] = useState([]);
+  const products = useSelector(state => state.products);
+  const cart = useSelector(state => state.cart);
+  const auth = useSelector(state => state.auth);
+  const dispatch = useDispatch();
+    
 
+    useEffect(() => {
+        getCategories().then(({ data }) => setCategories(data));
+    }, []);
 
-
-const TopBar = () => {
     return (
-        <header>
             <Navbar bg="dark" variant="dark" expand="sm" collapseOnSelect>
                 <Container>
-                    <NavbarBrand href="/">Computer Shop</NavbarBrand>
+                    <IndexLinkContainer to="/" exact>
+                        <NavbarBrand>Computer Shop</NavbarBrand>
+                    </IndexLinkContainer>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <NavDropdown title="Category" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Utama</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Laptop</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Deckstop</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.4">Aksesoris</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Details</NavDropdown.Item>
+                    <Nav className="mr-auto">
+                       <NavDropdown title={products.category || 'Kategori'} id="navbarScrollingDropdown">
+                        {
+                             categories.map((category, i) => (
+                                <NavDropdown.Item 
+                                    href="#" 
+                                    key={i} 
+                                    active={category.name === products.category} 
+                                    onClick={() => dispatch(setCategory(category.name))}
+                                >{category.name}</NavDropdown.Item>
+                                ))
+                            }
                         </NavDropdown>
-                        <Form className="d-flex">
-                            <FormControl  type="search" placeholder="Search" className="me-sm-2" />
-                            <Button variant="outline-success">Search</Button>
+                    </Nav>
+                        <Form className="d-sm-inline-flex">
+                            <FormControl  size="sm" type="search" placeholder="Search" className="me-sm-2" onChange={(e) => dispatch(setKeyword(e.target.value))} />
+                            <Button variant="outline-success" size="sm" type="submit">Search</Button>
                         </Form>
-                        <Nav className="ml-auto">
-                            <Nav.Link>
+                    <Nav className="ml-auto">
+                        <LinkContainer to="/cart">
+                            <Nav.Link className="mr-sm-2">
                                 <i className="fas fa fa-shopping-cart"></i>
-                                </Nav.Link>
-                            <Nav.Link>
+                                <span className="position-absolute top-10 start-100 translate-middle badge rounded-pill bg-danger">
+                                 { totalItemCart(cart) }
+                                    <span className="visually-hidden">Total item</span>
+                                </span>
+                            </Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to={auth.user ? '/profile' : '/login'}>
+                            <Nav.Link title="profil">
                                 <i className="fas fa fa-user"></i>
-                                </Nav.Link>
+                            </Nav.Link>
+                        </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
-           </Navbar>
-            </header>
-    )
-}
-
-export default TopBar;
+            </Navbar>
+    );
+};
 
 
